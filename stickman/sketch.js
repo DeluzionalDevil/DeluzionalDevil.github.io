91081d28d0c7f60ed749b7c0d05187249c04b68a
mathilde <mathildeandersen01@gmail.com>
2020-10-22 09:02:51 +0200

    tilføjet code

---
+++ stickman/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+  circle(200,130,90)
+  line(200,175,200,240)
+  triangle(200,240,170,310,230,310)
+  line(200,185,210,250)
+  line(200,185,190,250)
+  line (190,310,190,350)
+  line(210,310,210,350)
+  arc(200,140, 40, 40, 0, HALF_PI +  QUARTER_PI, CHORD);
+  circle(185,120,20)
+  circle(215,120,20)
+  circle(185,120,7)
+  circle(215,120,7)
+  line(185,110,182,105)
+  line(180,112,177,106.5)
+  line(175,117,172,110)
+  line(215,110,218,105)
+  line(220,112,223,106.5)
+  line(225,117,228,110)
+}
+
